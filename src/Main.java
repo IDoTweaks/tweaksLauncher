9f308f48dcd964f9bda4048725735d6986ec5e01
IDoTweaks <idotweaks@gmail.com>
2026-01-10 00:03:58 +0200

    added base version
    made this quickly it works fine still some problems tho WILL BE IMPROVING IT!

---
+++ src/Main.java
@@ -0,0 +1,4 @@
+void main() throws IOException {
+    //String json = "{\n \"name\": \"firefox\",\n \"path\": /usr/bin/firefox\n}"; Files.writeString(Path.of("data.json"), json);
+    new MyFrane();
+}
